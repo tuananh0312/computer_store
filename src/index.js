f57d2f2b01672f0dd00f631101739a01ea72bdb6
Minh <hoangminh200401@gmailgmail.com>
2022-10-02 13:22:31 +0700

    new update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import  'boxicons';
+import Layout from './components/Layout';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>
 );
 
